aca1306d5970e6257090580daff845f17c23150a
Alisina Bahadori <alisina.bm@gmail.com>
2020-03-19 04:25:54 +0330

    Add mikrotik_reset and remove_routes.sh for linux

--- remove_routes.sh
+++ remove_routes.sh
@@ -2,7 +2,6 @@
 
 ARGUMENT_LIST=(
     "country:"
-    "gateway:"
     "help"
 )
 
@@ -30,14 +29,13 @@ while [[ $# -gt 0 ]]; do
             ;;
 
 	--help)
-	  echo "This script helps you to add routes for a specific country."
+	  echo "This script helps you to remove routes for a specific country."
 	  echo ""
 	  echo "In order to run interactiveley just call this script without the --help switch."
 	  echo "This command requires superuser privilages! (MUST run as root)"
 	  echo ""
 	  echo "Arguments:"
 	  echo -e "\t--country\tCountry of your choice as 2-letter ISO code. Example: US, IR"
-	  echo -e "\t--gateway\tGateway used to route to these ip addresses. Example: 192.168.0.1 OR vpn0"
 	  echo -e "\t--help\t\tShows this help."
 	  exit
 	  ;;
@@ -57,21 +55,13 @@ else
 fi
 
 
-if [ -z ${gateway+x} ];
-then
-	read -p "Enter the gateway that you want to use for $countryiso (example 192.168.0.1 OR vpn0): " gateway
-else
-	echo "Gateway: $gateway"
-fi
-
 countryiso=$(echo "$countryiso" | tr '[:upper:]' '[:lower:]')
 
 input="db/country/$countryiso/ipv4.cidr"
 
 while IFS= read -r line
 do
-		echo "ip route add dst-address=$line gateway=$gateway">>mikrotik_script.txt
-		echo "ip route remove [find dst-address=$line gateway=$gateway]">>mikrotik_restore_script.txt
+	ip route del "$line"
 done < "$input"
 
 
